5fd5191bb5c0799b36fca071def51972c0ccf677
Yvonne Danisch <yvonnedanisch@gmx.de>
2022-09-18 12:11:21 +0200

    fix test and use AssertJ

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GildedRoseTest {
@@ -12,6 +13,6 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(app.items[0].name).isEqualTo("foo");
     }
 }
