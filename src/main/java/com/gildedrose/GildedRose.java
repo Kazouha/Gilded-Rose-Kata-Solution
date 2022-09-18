972bfe9afe40fbd2a27bd1a8140a1fed8ece2fbc
Yvonne Danisch <yvonnedanisch@gmx.de>
2022-09-18 13:49:41 +0200

    combine some if-statements

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,26 +11,20 @@ public class GildedRose {
         for (Item item : items) {
             if (!isAgedBree(item)
                     && !isBackstagePass(item)) {
-                if (item.quality > 0) {
-                    if (!isSulfuras(item)) {
-                        item.quality = item.quality - 1;
-                    }
+                if (item.quality > 0 && !isSulfuras(item)) {
+                    item.quality = item.quality - 1;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
                     if (isBackstagePass(item)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
@@ -43,13 +37,11 @@ public class GildedRose {
             if (item.sellIn < 0) {
                 if (!isAgedBree(item)) {
                     if (!isBackstagePass(item)) {
-                        if (item.quality > 0) {
-                            if (!isSulfuras(item)) {
-                                item.quality = item.quality - 1;
-                            }
+                        if (item.quality > 0 && !isSulfuras(item)) {
+                            item.quality = item.quality - 1;
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < 50) {
